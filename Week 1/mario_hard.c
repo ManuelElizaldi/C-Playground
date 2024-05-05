32f3169397bff329fefe69179f51ca71c106d679
Manuel Elizaldi <manuelelizaldib@gmail.com>
2024-05-05 15:56:04 -0500

    added some note comments

--- Week 1/mario_hard.c
+++ Week 1/mario_hard.c
@@ -28,23 +28,23 @@ int main(void)
 
 void print_row(int spaces, int bricks,int gap ,int blocks)
 {
-    for (int j = 0; j < spaces; j++)
+    for (int j = 0; j < spaces; j++) // n - i - 1 // spaces in fron of the pyramid, each row we want -1
     {
         printf(" ");
     }
-    for (int i = 0; i < bricks; i++)
+    for (int i = 0; i < bricks; i++) // i + 1 first side of the pyramid, each row we want 1 more
     {
         printf("#");
     }
-    for (int g = 0; g < gap; g++)
+    for (int g = 0; g < gap; g++) // g -> constant, we always want 2 spaces for the gap
     {
         printf(" ");
     }
-    for (int k = 0; k < blocks; k++)
+    for (int k = 0; k < blocks; k++) // other side of the pyramid
     {
         printf("#");
     }
-    printf("\n");
+    printf("\n"); // when all loops are done, we go to the next line - next iteration - i++ 
 }
 
 
